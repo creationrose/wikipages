c798e5706a57fda2f068571dc1c34fe30fe69843
jessica <ladyjtm@gmail.com>
2015-03-07 16:54:01 -0800

    Add new contacts to the page. Be sure to use strings that match the form, not symbols

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,17 @@ class ContactsController < ApplicationController
 		render('contacts/index.html.erb')
 	end
 
+	def new
+		render('contacts/new.html.erb')
+	end
+
+	def create
+		@contact = Contact.create(:name => params["contact_name"], :phone => params["contact_phone"], :email => params["contact_email"])
+		render('contacts/success.html.erb')
+	end
+
 	def show
-		@contact = Contact.find(params[:id])
+		@contact = Contact.find(params["id"])
 		render('contacts/show.html.erb')
 	end
 end
